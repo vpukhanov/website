5d647a2c862a82ab24f1a0a5b5dea91322b4d2fd
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2020-07-30 13:16:48 +0300

    adds privacy policy page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import GoogleFonts from "next-google-fonts";
 import Head from "next/head";
 
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
-import PersonalProjects from "../components/PersonalProjects";
-import WorkExperience from "../components/WorkExperience";
+import CommonHead from "../components/CommonHead";
 import Education from "../components/Education";
 import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import PersonalProjects from "../components/PersonalProjects";
+import WorkExperience from "../components/WorkExperience";
 
 export default function Home() {
   return (
@@ -14,33 +15,11 @@ export default function Home() {
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
       <Head>
         <title>Vyacheslav Pukhanov</title>
-        <meta name="charset" content="utf-8" />
         <meta
           name="description"
           content="Vyacheslav Pukhanov is a professinal web developer and a hobbyist mobile developer based in Yaroslavl, Russia"
         />
-        <link rel="icon" href={"/favicon.ico"} />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={"/apple-touch-icon.png"}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={"/favicon-32x32.png"}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={"/favicon-16x16.png"}
-        />
-        <link rel="manifest" href={"/site.webmanifest"} />
-        <link rel="mask-icon" href={"/safari-pinned-tab.svg"} color="#070798" />
-        <meta name="msapplication-TileColor" content="#070798" />
-        <meta name="theme-color" content="#070798" />
+        <CommonHead />
       </Head>
 
       <Navbar />
---
+++ pages/privacy/stops-app.js
@@ -0,0 +1,60 @@
+import GoogleFonts from "next-google-fonts";
+import Head from "next/head";
+
+import CommonHead from "../../components/CommonHead";
+
+export default function StopsAppPrivacyPolicy() {
+  return (
+    <div className="app-container">
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
+      <Head>
+        <title>Stops App Privacy Policy | Vyacheslav Pukhanov</title>
+        <meta
+          name="description"
+          content="Privacy Policy for the Stops: Yaroslavl's Transport app"
+        />
+        <CommonHead />
+      </Head>
+      <section className="privacy-en">
+        <h1 className="text-title">Privacy Policy</h1>
+        <p className="text-body location">
+          The "Stops: Yaroslavl's Transport" processes your location locally and
+          on-device. We may collect anonymous information about your actions in
+          the app, simply to know which features of the app are used the most,
+          so we can focus on them during development.
+        </p>
+        <p className="text-body third-party">
+          The only information that is sent to the information providers{" "}
+          <b>yartr.ru</b> and <b>ot76.ru</b>
+          by the app is the stop currently selected in the application and the
+          list of routes selected by you in the app. Whether they may use and
+          store this data is subject to their own privacy policy.
+        </p>
+        <p className="text-body text-sublime">
+          <em>Last update: April 18, 2020</em>
+        </p>
+      </section>
+      <hr />
+      <section className="privacy-ru">
+        <h2 className="text-title">Политика конфиденциальности</h2>
+        <p className="text-body location">
+          Приложение "Остановки: транспорт Ярославля" обрабатывает ваше
+          местоположение локально, на вашем устройстве. Мы можем собирать
+          анонимную статистику о действиях, выполняемых вами в приложении, чтобы
+          знать, какие функции приложения используются наиболее часто, и
+          концентрировать силы на ух улучшении.
+        </p>
+        <p className="text-body third-party">
+          Поставщикам информациии <b>yartr.ru</b> и <b>ot76.ru</b> отправляются
+          только данные о выбранной в настоящий момент остановке и список
+          отслеживаемых вами на карте маршрутов. Как они могут использовать и
+          хранить эти данные - предмет их собственной политики
+          конфиденциальности.
+        </p>
+        <p className="text-body text-sublime">
+          <em>Последнее обновление: 18 апреля 2020</em>
+        </p>
+      </section>
+    </div>
+  );
+}
