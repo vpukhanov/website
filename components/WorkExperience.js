1b0af9cf7accd12e379c8ed9c78f6314ed8a8510
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-07-10 08:17:28 +0300

    add tinkoff experience

--- components/WorkExperience.js
+++ components/WorkExperience.js
@@ -4,7 +4,22 @@ export default function WorkExperience() {
       <h2 className="text-subtitle">Work Experience</h2>
       <div>
         <h3 className="text-caption">
-          Krista RPA <span className="text-sublime">2020 &mdash; today</span>
+          JSC Tinkoff Bank <span className="text-sublime">2021 &mdash; today</span>
+          <p className="text-body">Senior frontend engineer on the media projects team</p>
+          <p className="text-body">Key technologies:</p>
+          <ul className="text-body">
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Server-side rendering</li>
+            <li>Docker</li>
+            <li>Git</li>
+          </ul>
+        </h3>
+      </div>
+      <div>
+        <h3 className="text-caption">
+          Krista RPA <span className="text-sublime">2020 &mdash; 2021</span>
           <p className="text-body">Member of the core frontend team</p>
           <p className="text-body">Key technologies:</p>
           <ul className="text-body">
