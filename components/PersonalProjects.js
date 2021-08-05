9a50f8f0d17e2a884f28faa74ca9271660b79f1e
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-08-05 21:48:21 +0300

    remove Binbox personal project

--- components/PersonalProjects.js
+++ components/PersonalProjects.js
@@ -31,33 +31,6 @@ export default function PersonalProjects() {
           <li>Postfix + Dovecot</li>
         </ul>
       </div>
-      <div>
-        <p className="text-caption">
-          <a
-            href="https://apps.apple.com/us/app/stops-yaroslavls-transport/id1483241565"
-            target="_blank"
-            rel="noopener"
-          >
-            Stops: Yaroslavl's Transport
-          </a>
-          &nbsp;
-          <span className="text-sublime">for iOS</span>
-        </p>
-        <p className="text-body">
-          Stops app displays the schedule of Yaroslavl’s public transport at the
-          stop that is closest to the user. It’s fully automated, removing the
-          need to manually dig through a long list of stops. Transport routes
-          can be added to favorites to make their schedule visible at a glance,
-          as well as to sync them to the Apple Watch companion app. The users
-          also get access to the live transport location map with predicted
-          arrival times.
-        </p>
-        <p className="text-body">Key technologies:</p>
-        <ul className="text-body">
-          <li>Swift</li>
-          <li>UIKit</li>
-        </ul>
-      </div>
     </section>
   );
 }
