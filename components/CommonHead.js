4dff10047ddca0049cedb8dca7ee52452d0c0079
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-08-06 16:08:57 +0300

    update next-js

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -2,7 +2,11 @@ export default function CommonHead() {
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
         rel="stylesheet"
