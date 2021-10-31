2339053ddb345c7d536defb76a2a78ec3a26e9ec
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-10-31 20:39:37 +0300

    Update unidue.js

--- pages/privacy/unidue.js
+++ pages/privacy/unidue.js
@@ -13,7 +13,8 @@ export default function UnidueAppPrivacyPolicy() {
         <CommonHead />
       </Head>
       <section className="privacy-en" style={{ maxWidth: 600 }}>
-        <h1 className="text-title">✅ Privacy First</h1>
+        <h1 className="text-title">Privacy Policy</h1>
+        <h2 className="text-subtitle">✅ Privacy First</h2>
         <ul className="text-body">
           <li>All of your info stays on your device</li>
           <li>The app works completely offline</li>
@@ -22,11 +23,18 @@ export default function UnidueAppPrivacyPolicy() {
             Apple's iCloud itself, with no direct developer access
           </li>
         </ul>
-        <h2>🐱 RevenueCat</h2>
+        <h2 className="text-subtitle">🐱 RevenueCat</h2>
         <p className="text-body">
           Unidue uses RevenueCat to verify App Store receipts, process in-app
           purchases, and restore previous purchases. RevenueCat does not have
           access to any personal user information.
+        </p>
+        <h2 className="text-subtitle">🤖 TelemetryDeck</h2>
+        <p className="text-body">
+          Unidue uses TelemetryDeck to understand how users use the app so that
+          we can improve commonly used functionality, make it easier to use and
+          discover. TelemetryDeck does not have access to any personal user
+          information.
         </p>
       </section>
     </div>
