22ef5f1b0ff28c4977d97133e077d1acffd84db4
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-01-12 17:52:27 +0000

    add unidue terms of service

---
+++ pages/tos/unidue.js
@@ -0,0 +1,193 @@
+import Head from 'next/head'
+import CommonHead from '../../components/CommonHead'
+
+export default function UnidueAppTermsOfService() {
+  return (
+    <div className='app-container'>
+      <Head>
+        <title>Unidue App Terms of Service | Vyacheslav Pukhanov</title>
+        <meta name='description' content='Terms of Service for Unidue' />
+        <CommonHead />
+      </Head>
+      <section className='privacy-en'>
+        <h1 className='text-title'>Terms of Service</h1>
+        <p className='text-body'>
+          The Unidue app is a copyrighted work belonging to Vyacheslav Pukhanov
+          (the Developer). Certain features of the App may be subject to
+          additional guidelines, terms, or rules, which will be posted on this
+          page in connection with such features.
+        </p>
+        <p className='text-body'>
+          These Terms of Use describe the legally binding terms and conditions
+          that oversee your use of the App. BY USING THE APP, YOU ARE BEING
+          COMPLIANT THAT THESE TERMS and you represent that you have the
+          authority and capacity to enter into these Terms. YOU SHOULD BE AT
+          LEAST 18 YEARS OF AGE TO ACCESS THE APP. IF YOU DISAGREE WITH ANY OF
+          THE PROVISIONS OF THESE TERMS, DO NOT USE THE APP.
+        </p>
+        <h2 className='text-subtitle'>Apple Terms</h2>
+        <p className='text-body'>
+          This agreement is concluded between You and the Developer only, and
+          not with Apple, and the Developer, not Apple, is solely responsible
+          for the Licensed Application and the content thereof. The EULA may not
+          provide for usage rules for Licensed Applications that are in conflict
+          with, the Apple Media Services Terms and Conditions as of the
+          Effective Date.
+        </p>
+        <p className='text-body'>
+          The license granted to You for the Unidue Application is limited to a
+          non-transferable license to use the Licensed Application on any
+          Apple-branded Products that You own or control and as permitted by the
+          Usage Rules set forth in the Apple Media Services Terms and
+          Conditions, except that such Licensed Application may be accessed and
+          used by other accounts associated with the purchaser via volume
+          purchasing.
+        </p>
+        <p className='text-body'>
+          The developer is solely responsible for providing any maintenance and
+          support services with respect to the Unidue Application, as specified
+          in this terms, or as required under applicable law. You acknowledge
+          that Apple has no obligation whatsoever to furnish any maintenance and
+          support services with respect to the Licensed Application. (See
+          "Maintenance" section)
+        </p>
+        <p className='text-body'>
+          The developer solely responsible for any product warranties, whether
+          express or implied by law, to the extent disclaimed by the
+          "Disclaimers" section of this agreement. In the event of any failure
+          of the Licensed Application to conform to any applicable warranty, You
+          may notify Apple, and Apple will refund the purchase price for the
+          Licensed Application to You; and You acknowledge that, to the maximum
+          extent permitted by applicable law, Apple will have no other warranty
+          obligation whatsoever with respect to the Licensed Application, and
+          any other claims, losses, liabilities, damages, costs or expenses
+          attributable to any failure to conform to any warranty will be
+          Developer's sole responsibility.
+        </p>
+        <p className='text-body'>
+          You must acknowledge that Developer, not Apple, is responsible for
+          addressing any claims of the End-User or any third party relating to
+          the Licensed Application or the end-user’s possession and/or use of
+          that Licensed Application, including, but not limited to: (i) product
+          liability claims; (ii) any claim that the Licensed Application fails
+          to conform to any applicable legal or regulatory requirement; and
+          (iii) claims arising under consumer protection, privacy, or similar
+          legislation, including in connection with Your Licensed Application’s
+          use of the HealthKit and HomeKit frameworks. The EULA may not limit
+          Developer's liability to the End-User beyond what is permitted by
+          applicable law.
+        </p>
+        <p className='text-body'>
+          You must acknowledge that, in the event of any third party claim that
+          the Licensed Application or Your possession and use of that Licensed
+          Application infringes that third party’s intellectual property rights,
+          the Developer, not Apple, will be solely responsible for the
+          investigation, defense, settlement and discharge of any such
+          intellectual property infringement claim.
+        </p>
+        <p className='text-body'>
+          Developer, Vyacheslav Pukhanov, can be contacted via email at{' '}
+          <a href='mailto:vyacheslav@pukhanov.ru'>vyacheslav@pukhanov.ru</a>
+        </p>
+        <p className='text-body'>
+          You must represent and warrant that (i) you are not located in a
+          country that is subject to a U.S. Government embargo, or that has been
+          designated by the U.S. Government as a “terrorist supporting” country;
+          and (ii) you are not listed on any U.S. Government list of prohibited
+          or restricted parties.
+        </p>
+        <p className='text-body'>
+          You acknowledge and agree that Apple, and Apple’s subsidiaries, are
+          third party beneficiaries of the EULA, and that, upon Your acceptance
+          of the terms and conditions of the EULA, Apple will have the right
+          (and will be deemed to have accepted the right) to enforce the EULA
+          against You as a third party beneficiary thereof.
+        </p>
+        <h2 className='text-subtitle'>Access to the App</h2>
+        <p className='text-body'>
+          <b>Subject to these Terms.</b> Developer grants you a
+          non-transferable, non-exclusive, revocable, limited license to access
+          the App solely for your own personal, noncommercial use.
+        </p>
+        <p className='text-body'>
+          <b>Certain Restrictions.</b> The rights approved to you in these Terms
+          are subject to the following restrictions: (a) you shall not sell,
+          rent, lease, transfer, assign, distribute, host, or otherwise
+          commercially exploit the App; (b) you shall not change, make
+          derivative works of, disassemble, reverse compile or reverse engineer
+          any part of the App; (c) you shall not access the App in order to
+          build a similar or competitive website; and (d) except as expressly
+          stated herein, no part of the App may be copied, reproduced,
+          distributed, republished, downloaded, displayed, posted or transmitted
+          in any form or by any means unless otherwise indicated, any future
+          release, update, or other addition to functionality of the App shall
+          be subject to these Terms. All copyright and other proprietary notices
+          on the App must be retained on all copies thereof.
+        </p>
+        <p className='text-body'>
+          Developer reserves the right to change, suspend, or cease the App with
+          or without notice to you. You approved that Developer will not be held
+          liable to you or any third-party for any change, interruption, or
+          termination of the App or any part.
+        </p>
+        <p className='text-body'>
+          <b>No Support or Maintenance.</b> You agree that Developer will have
+          no obligation to provide you with any support in connection with the
+          Site.
+        </p>
+        <h2 className='text-subtitle'>Disclaimers</h2>
+        <p className='text-body'>
+          The App is provided on an “as-is” and “as available” basis, and
+          Developer expressly disclaims any and all warranties and conditions of
+          any kind, whether expressed, implied, or statutory, including all
+          warranties or conditions of merchantability, fitness for a particular
+          purpose, title, quiet enjoyment, accuracy, or non-infringement. We
+          make not guarantee that the App will meet your requirements, will be
+          available on an uninterrupted, timely, secure, or error-free basis, or
+          will be accurate, reliable, free of viruses or other harmful code,
+          complete, legal, or safe. If applicable law requires any warranties
+          with respect to the site, all such warranties are limited in duration
+          to ninety (90) days from the date of first use.
+        </p>
+        <p className='text-body'>
+          Some jurisdictions do not allow the exclusion of implied warranties,
+          so the above exclusion may not apply to you. Some jurisdictions do not
+          allow limitations on how long an implied warranty lasts, so the above
+          limitation may not apply to you.
+        </p>
+        <h2 className='text-subtitle'>Limitation on Liability</h2>
+        <p className='text-body'>
+          To the maximum extent permitted by law, in no event shall the
+          developer be liable to you or any third-party for any lost profits,
+          lost data, costs of procurement of substitute products, or any
+          indirect, consequential, exemplary, incidental, special or punitive
+          damages arising from or relating to these terms or your use of, or
+          incapability to use the App even if the Developer has been advised of
+          the possibility of such damages. Access to and use of the App is at
+          your own discretion and risk, and you will be solely responsible for
+          any damage to your device or computer system, or loss of data
+          resulting therefrom.
+        </p>
+        <p className='text-body'>
+          To the maximum extent permitted by law, notwithstanding anything to
+          the contrary contained herein, our liability to you for any damages
+          arising from or related to this agreement, will at all times be
+          limited to a maximum of fifty U.S. dollars (u.s. $50). The existence
+          of more than one claim will not enlarge this limit.
+        </p>
+        <p className='text-body'>
+          Some jurisdictions do not allow the limitation or exclusion of
+          liability for incidental or consequential damages, so the above
+          limitation or exclusion may not apply to you.
+        </p>
+        <p className='text-body text-sublime'>
+          Contact: Vyacheslav Pukhanov,{' '}
+          <a href='mailto:vyacheslav@pukhanov.ru'>vyacheslav@pukhanov.ru</a>
+        </p>
+        <p className='text-body text-sublime'>
+          <em>Last update: January 12, 2022</em>
+        </p>
+      </section>
+    </div>
+  )
+}
